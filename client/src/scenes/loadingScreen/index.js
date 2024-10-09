da6f63a659b2566a50b571603e32c272a6e32254
MaksimArtemev <falconte7767@gmail.com>
2024-10-09 19:19:37 -0400

    Add couple more options for loading screen

--- client/src/scenes/loadingScreen/index.js
+++ client/src/scenes/loadingScreen/index.js
@@ -9,7 +9,7 @@ const LoadingScreen = () => {
         // Wait for the GIF to finish one cycle, then navigate
         const timer = setTimeout(() => {
             navigate('/map');
-        }, 3050);
+        }, 6050);
 
         // Clean up the timer if the component unmounts
         return () => clearTimeout(timer);
@@ -18,7 +18,7 @@ const LoadingScreen = () => {
     return (
         <div style={{ height: '100vh', width: '100vw', overflow: 'hidden' }}>
             <img 
-                src="https://media.giphy.com/media/2JcU7di1fv9gZoousy/giphy.gif?cid=ecf05e478sdvi638uj23ye0ha40ut8kh4ko3bm54mjd8old0&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+                src="https://media.giphy.com/media/EURa8GpC4Kyx0jGWGB/giphy.gif?cid=ecf05e47rbbd8aodgespv2wb98k6kybfqr32fv9t2d4ytdr0&ep=v1_gifs_search&rid=giphy.gif&ct=g"
                 alt="Loading"
                 style={{
                     width: '100%',
@@ -26,6 +26,48 @@ const LoadingScreen = () => {
                     objectFit: 'cover'
                 }}
             />
+            
+            {/* OPTION 2 FOOTBALL FIELD ANIMATED
+                <img 
+                src="https://media.giphy.com/media/LOF4krQGVu6M5wQM2z/giphy.gif?cid=ecf05e478582rz6ivd9ezne9gz6oa304xv0avhtvfq267y1a&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+                alt="Loading"
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover'
+                }}
+            /> */}
+
+            {/* OPTION 3 MAP ROUTE
+                <img 
+                src="https://media.giphy.com/media/FoMJ3wPyRsGVwRtbKC/giphy.gif?cid=ecf05e47eazq0vqcnwu41w0vb7mdl2fzf8w1v6789otwpnls&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+                alt="Loading"
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover'
+                }}
+            /> */}
+            {/* OPTION 4 TABLE LOOP
+                <img 
+                src="https://media.giphy.com/media/3oEjHRefD1rpcXUOkM/giphy.gif?cid=ecf05e472nl8a8tyf9awdvbrzuevsyukr10hgcv4x24zj5j6&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+                alt="Loading"
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover'
+                }}
+            /> */}
+            {/* OPTION 5 EMOTICON thredmill TABLING
+                <img 
+                src="https://media.giphy.com/media/IboGSjkXaOre0/giphy.gif?cid=ecf05e47rz97c3des57mogo1phgx78npbh3w1os9rvbp1xxj&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+                alt="Loading"
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover'
+                }}
+            /> */}
         </div>
     )
 }
