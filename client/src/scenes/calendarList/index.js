5ae21d4ed2a164b38db65c1a0a122995717c10b0
me <lopezbeto305@gmail.com>
2024-10-11 14:13:03 -0400

    Added calendar not fully done

--- client/src/scenes/calendarList/index.js
+++ client/src/scenes/calendarList/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import './CalendarList.css';
 
-const CalendarList = () => {
+const CalendarComponent = () => {
   return (
-    <div>CalendarList page here</div>
-  )
-}
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      initialView="timeGridWeek" // Use timeGrid view to show the now indicator
+      headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      }}
+      nowIndicator={true} // Show the current time indicator
+      editable={true}
+      selectable={true}
+    />
+  );
+};
 
-export default CalendarList
+export default CalendarComponent;
