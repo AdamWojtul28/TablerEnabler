a41125ea1d83770e7a198dae95c70566610d2b63
mvorot <mikevorot@gmail.com>
2024-10-11 17:23:47 -0400

    Cleaned up map file

--- client/src/scenes/map/index.jsx
+++ client/src/scenes/map/index.jsx
@@ -5,7 +5,7 @@ import { Icon, divIcon, point } from "leaflet";
 import MarkerClusterGroup from 'react-leaflet-cluster';
 
 
-export default function App() {
+export default function Map() {
   // constant markers
   const markers = [
     {
@@ -66,34 +66,3 @@ const createClusterCustomIcon = function (cluster) {
     </MapContainer>
   );
 }
-
-// ***************** Max's  code below *****************************
-// import React from 'react'
-
-// const Map = () => {
-//   return (
-//     <div>
-//       <div style={{fontWeight: 'bold', fontSize: '32px'}}>Map page goes here</div>
-//       <br></br><br></br><br></br><br></br>
-//       <div>Ready for development my MOKOKOS</div><br></br>
-
-//       <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
-//         <li>Done: Need to disable top nav bar for smaller screens and vice versa bottom nav bar for bigger screens</li>
-//         <li>Menu item icon from top nav bar is clickable now</li>
-//         <li>Sidebar is created and functional with ListItems leading to their corresponding url thru substring parsing</li>
-//         <li style={{ marginLeft: '20px' }}>User info section at the bottom of Sidebar is NOT clickable as of now</li>
-//         <li>TopNavbar and Sidebar are visible only on 600px+ screen width</li>
-//         <li>BottomNavbar is visible only on 600px- screen width</li>
-//         <li>Add 6 different loading page options (NEED HELP CHOOSING BETWEEN THEM - I can modify them based on ur inputs! )</li>
-//       </ul>
-
-//       <br></br>
-//       <br></br>
-
-//       <img src="https://i.redd.it/jzugmg96wyg81.gif" alt="Mokoko GIF" />
-
-//     </div>
-//   )
-// }
-
-// export default Map
