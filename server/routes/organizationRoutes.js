354923c0c679bfccdd0a8814a8cfd3865bce0117
MaksimArtemev <falconte7767@gmail.com>
2024-10-27 17:43:43 -0400

    Work on authentication. Add routes for authentication, student, organization, admin. Add middleware for authentication and roles. Add JWTPRIVATEKEY and SALT into private variable aka .env

---
+++ server/routes/organizationRoutes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const { User, validate } = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const auth = require("../MiddleWare/authMiddleware");
+
+// Register new user
+router.post("/", async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error)
+            return res.status(400).send({ message: error.details[0].message });
+
+        const user = await User.findOne({ email: req.body.email });
+        if (user)
+            return res
+                .status(409)
+                .send({ message: "User with given email already exists!" });
+
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        const newUser = new User({ ...req.body, password: hashPassword });
+        await newUser.save();
+
+        const token = newUser.generateAuthToken();
+        res.status(201).send({ data: token, message: "User created successfully" });
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
+
+router.get("/me", auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+    res.send(user);
+});
+
+
+module.exports = router;
