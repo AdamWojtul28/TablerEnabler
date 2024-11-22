ddb93389e6d3d38a84b9f14305ef61bb05cb0d92
AdamWojtul28 <adamwojtulewski@ufl.edu>
2024-11-21 19:00:28 -0500

    Webscraping Progress and Officer Schema

---
+++ server/models/Officer.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const officerSchema = new mongoose.Schema({
+  org_name: { type: String, required: true },
+  ufl_email: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+officerSchema.index({ org_name: 1, ufl_email: 1 }, { unique: true });
+
+const Officer = mongoose.model("Officer", officerSchema);
+export default Officer;
